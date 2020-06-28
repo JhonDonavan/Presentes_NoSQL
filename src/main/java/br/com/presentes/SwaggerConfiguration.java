021283e0716656af5b46712549d9009bce609533
Jhon Donavan <jhondonavan@gmail.com>
2020-06-28 16:42:34 -0300

    security spring and jwt

--- src/main/java/br/com/presentes/SwaggerConfiguration.java
+++ src/main/java/br/com/presentes/SwaggerConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.presentes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,13 +10,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.swagger.models.auth.In;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.service.TokenEndpoint;
+import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -34,9 +48,11 @@ public class SwaggerConfiguration {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("br.com.presentes")).paths(PathSelectors.any()).build()
-				.apiInfo(metaInfo());
+				.useDefaultResponseMessages(false)
+		        .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+		        .securityContexts(Arrays.asList(securityContext()));
 	}
-
+	
 	private ApiInfo metaInfo() {
 
 		ApiInfo apiInfo = new ApiInfo("Presentes", "DESCRI��O DO PROJETO", "1.0", "Terms of Service",
@@ -45,6 +61,19 @@ public class SwaggerConfiguration {
 
 		return apiInfo;
 	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.ant("/api/**"))
+				.build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("ADMIN", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Token Access", authorizationScopes));
+	}
+
 
 	// for @EnableWebMvc
 	/*
